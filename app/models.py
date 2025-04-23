4cbe995b3757f72a46a9c8dbf8440cd69c70c8aa
kinjalksahu <kinjalksahu07@gmail.com>
2025-04-23 13:46:31 +0530

    do changes in settings.py

--- app/models.py
+++ app/models.py
@@ -67,7 +67,7 @@ class Product(models.Model):
     description = models.TextField() 
     brand = models.CharField(max_length=100) 
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)  
-    product_image = models.ImageField(upload_to='product/')  
+    product_image = models.ImageField(upload_to='productimg/')  
     def __str__(self): 
         return self.title  # More readable output
 
--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'c:/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/profile/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
